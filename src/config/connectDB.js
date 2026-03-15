d02d64a9dc8735452d5172285bd8d990ee938bc3
Phantom1224 <lawrencegabrieltorres9@gmail.com>
2026-03-15 10:01:25 +0800

    added mongo dns servers for online mongodb server compatibility

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,8 +1,20 @@
 const mongoose = require("mongoose");
+const dns = require("dns");
 
 async function connectDB(mongoUri) {
   if (!mongoUri) {
     throw new Error("MONGO_URI is not set");
+  }
+
+  const dnsServersRaw = process.env.MONGO_DNS_SERVERS;
+  if (dnsServersRaw) {
+    const servers = dnsServersRaw
+      .split(",")
+      .map((server) => server.trim())
+      .filter(Boolean);
+    if (servers.length > 0) {
+      dns.setServers(servers);
+    }
   }
 
   await mongoose.connect(mongoUri);
